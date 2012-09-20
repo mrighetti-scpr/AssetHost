e40206da5cdd4439663a6c7e31a16df8d4ac876d
Bryan Ricker <bricker88@gmail.com>
2012-09-20 14:25:33 -0700

    Update UtilityController

--- app/controllers/asset_host_core/api/utility_controller.rb
+++ app/controllers/asset_host_core/api/utility_controller.rb
@@ -11,24 +11,36 @@ module AssetHostCore
     
       # see if we have a loader for this URL
       if asset = AssetHostCore.as_asset(params[:url])
-        if params[:note] && param[:note] != ''
+        if params[:note] && params[:note] != ''
           asset.notes += "\n#{params[:note]}"
-          asset.save()
         end
       
         # set hidden flag if desired
         if params[:hidden] && params[:hidden] != ""
           asset.is_hidden = true
-          asset.save()
         end
-      
+        
+        if params[:caption] && params[:caption] != ""
+          asset.caption = params[:caption]
+        end
+        
+        if params[:owner] && params[:owner] != ""
+          asset.owner = params[:owner]
+        end
+        
+        if params[:title] && params[:title] != ""
+          asset.title = params[:title]
+        end
+        
+        asset.save
+              
         render :json => { 
-          :id => asset.id, 
-          :title => asset.title, 
-          :caption => asset.caption, 
-          :owner => asset.owner,
-          :tags => asset.image.tags,
-          :size => [asset.image_width,asset.image_height].join('x')
+          :id        => asset.id, 
+          :title     => asset.title,
+          :caption   => asset.caption, 
+          :owner     => asset.owner,
+          :tags      => asset.image.tags,
+          :size      => [asset.image_width,asset.image_height].join('x')
         }
       else
         render :json => { :error => "Unable to find or load an asset at the URL #{params[:url]}" }, :status => :not_found
