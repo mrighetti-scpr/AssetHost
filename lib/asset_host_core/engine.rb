0a52269a27951b0edf69c65a828e3f67c83904b2
Bryan Ricker <bricker88@gmail.com>
2013-05-10 15:40:22 -0700

    Don't set Resque.redis here - the host app should do it

--- lib/asset_host_core/engine.rb
+++ lib/asset_host_core/engine.rb
@@ -23,18 +23,6 @@ module AssetHostCore
     # initialize our config hash
     config.assethost = ActiveSupport::OrderedOptions.new
     
-    # -- configure resque -- #
-    
-    initializer :set_up_assethost_resque do 
-      app_config = "#{Rails.application.root}/config/resque.yml"
-            
-      # use app resque config if it exists, otherwise supply our own
-      resque_config = YAML.load_file(
-        File.exists?(app_config) ? app_config : File.expand_path('../../../config/resque.yml',__FILE__)
-      )
-      Resque.redis = resque_config[Rails.env]
-    end
-    
     # -- post-initialization setup -- #
 
     config.after_initialize do
