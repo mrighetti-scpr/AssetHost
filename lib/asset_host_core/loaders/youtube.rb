27cfd49525fe5c4604b432935da6291b285a607f
Bryan Ricker <bricker88@gmail.com>
2013-05-20 09:15:48 -0700

    Add youtube import + spec

--- lib/asset_host_core/loaders/youtube.rb
+++ lib/asset_host_core/loaders/youtube.rb
@@ -6,30 +6,72 @@ module AssetHostCore
       #----------------
 
       def self.try_url(url)
-        url.match(/youtube\.com\/watch\?.*v=(?<id>\w+)/i) do |m|
+        url.match(/youtube\.com\/watch\?.*v=(?<id>[\w-]+)/i) do |m|
           self.new(url: url, id: m[:id])
         end
       end
 
       #----------------
 
-      def fetch_data
-        connection.get do |request|
-          request.url "/feeds/api/videos/#{@id}"
-          request.params['v'] = 2
-        end
+      def load
+        data = fetch_data
+
+        video = data["items"][0]
+        return nil if !video
+
+        snippet = video["snippet"]
+        return nil if !snippet
+
+        asset = AssetHostCore::Asset.new(
+          :image          => image_file(snippet["thumbnails"]["high"]["url"]),
+          :title          => snippet["title"],
+          :caption        => snippet["description"],
+          :url            => @url,
+          :owner          => "#{snippet["channelTitle"]} (via YouTube)",
+          :notes          => "Imported from YouTube: #{@url}",
+          :image_taken    => snippet["publishedAt"]
+        )
+
+        asset.save
+        asset
       end
 
+      #----------------
+
+      def fetch_data
+        response = client.execute!(
+          :api_method => youtube.videos.list,
+          :parameters => {
+            :id   => @id,
+            :part => 'id,snippet,contentDetails,player'
+          }
+        )
+
+        JSON.parse(response.body)
+      end
+
+      #----------------
 
       private
 
-      def connection
-        @connection ||= begin
-          Faraday.new('http://gdata.youtube.com') do |conn|
-            conn.response :xml
-            conn.adapter Faraday.default_adapter
-          end
+      def image_file(url)
+        @image_file ||= begin
+          uri = URI.parse(url)
+          response = Faraday.get(url)
+          file = Tempfile.new("IAYouTube", encoding: 'ascii-8bit')
+          file << response.body
         end
+      end
+
+      def client
+        @client ||= Google::APIClient.new(
+          :key              => AssetHostCore.config.google_api_key,
+          :authorization    => nil
+        )
+      end
+
+      def youtube
+        @youtube ||= client.discovered_api('youtube', 'v3')
       end
     end
   end
--- spec/lib/asset_host_core/loaders/youtube_spec.rb
+++ spec/lib/asset_host_core/loaders/youtube_spec.rb
@@ -1,4 +1,47 @@
 require 'spec_helper'
 
 describe AssetHostCore::Loaders::YouTube do
+  describe '::try_url' do
+    it 'matches a youtube URL' do
+      loader = AssetHostCore::Loaders::YouTube.try_url('http://www.youtube.com/watch?v=y8Kyi0WNg40')
+      loader.should be_a AssetHostCore::Loaders::YouTube
+    end
+
+    it 'does not match other stuff' do
+      loader = AssetHostCore::Loaders::YouTube.try_url('nope.com/nope')
+      loader.should eq nil
+    end
+  end
+
+  describe '#load' do
+    before :each do
+      FakeWeb.register_uri(:get, %r{googleapis\.com/discovery},
+        content_type: "application/json", body: load_api_response('youtube/discovery.json'))
+
+      FakeWeb.register_uri(:get, %r{googleapis\.com/youtube},
+        content_type: "application/json", body: load_api_response('youtube/video.json'))
+
+      FakeWeb.register_uri(:get, %r{i\.ytimg\.com},
+        content_type: "image/jpeg", body: load_image('chipmunk.jpg'))
+
+      @loader = AssetHostCore::Loaders::YouTube.try_url('http://www.youtube.com/watch?v=y8Kyi0WNg40')
+      @asset  = @loader.load
+    end
+
+    it 'creates a new asset' do
+      @asset.should be_a AssetHostCore::Asset
+    end
+
+    it 'sets the owner' do
+      @asset.owner.should match /cregets/
+    end
+
+    it 'sets the title' do
+      @asset.title.should eq "Dramatic Chipmunk"
+    end
+
+    it 'sets the caption' do
+      @asset.caption.should match /best 5 second clip/
+    end
+  end
 end
