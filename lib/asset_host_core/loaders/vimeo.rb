39500495b350e0d046c475ca96a55c93d8cb0fd1
Bryan Ricker <bricker88@gmail.com>
2013-05-20 09:15:54 -0700

    Begin work on vimeo import

--- lib/asset_host_core/loaders/vimeo.rb
+++ lib/asset_host_core/loaders/vimeo.rb
@@ -1,6 +1,35 @@
 module AssetHostCore
   module Loaders
     class Vimeo < Base
+      SOURCE = "Vimeo"
+
+      #----------------
+
+      def self.try_url(url)
+        url.match(/vimeo\.com\/(?<id>\d+)/i) do |m|
+          self.new(url: url, id: m[:id])
+        end
+      end
+
+      #----------------
+
+      def fetch_data
+        connection.get do |request|
+          request.url "video/#{@id}.json"
+        end
+      end
+
+
+      private
+
+      def connection
+        @connection ||= begin
+          Faraday.new('http://vimeo.com/api/v2') do |conn|
+            conn.response :json
+            conn.adapter Faraday.default_adapter
+          end
+        end
+      end
     end
   end
 end
