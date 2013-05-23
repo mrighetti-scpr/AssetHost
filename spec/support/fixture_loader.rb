12ff07d27e4ebbb4644353cd29941527ae9b7afc
Bryan Ricker <bricker88@gmail.com>
2013-05-22 19:56:58 -0700

    Switch back to File.join

--- spec/support/fixture_loader.rb
+++ spec/support/fixture_loader.rb
@@ -1,9 +1,9 @@
 module FixtureLoader
   def load_image(filename)
-    File.read(File.expand_path("../fixtures/images/#{filename}", File.dirname(__FILE__)))
+    File.read(File.join('spec', 'fixtures', 'images', filename))
   end
 
   def load_api_response(filename)
-    File.read(File.expand_path("../fixtures/api/#{filename}", File.dirname(__FILE__)))
+    File.read(File.join('spec', 'fixtures', 'api', filename))
   end
 end
