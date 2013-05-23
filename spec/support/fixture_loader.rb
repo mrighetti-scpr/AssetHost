c46fd4274c6ff2454acda7c815ae427e9a24b752
Bryan Ricker <bricker88@gmail.com>
2013-05-22 19:44:28 -0700

    Try to outsmart the load paths

--- spec/support/fixture_loader.rb
+++ spec/support/fixture_loader.rb
@@ -1,9 +1,9 @@
 module FixtureLoader
   def load_image(filename)
-    File.read(File.join('spec', 'fixtures', 'images', filename))
+    File.read(File.expand_path("../fixtures/images/#{filename}", File.dirname(__FILE__)))
   end
 
   def load_api_response(filename)
-    File.read(File.join('spec', 'fixtures', 'api', filename))
+    File.read(File.expand_path("../fixtures/api/#{filename}", File.dirname(__FILE__)))
   end
 end
