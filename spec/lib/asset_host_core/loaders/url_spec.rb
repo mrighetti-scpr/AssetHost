b6b8f3de29b31f307a8d9d65228f768417be1986
Bryan Ricker <bricker88@gmail.com>
2013-05-22 19:28:52 -0700

    Logging

--- spec/lib/asset_host_core/loaders/url_spec.rb
+++ spec/lib/asset_host_core/loaders/url_spec.rb
@@ -28,8 +28,7 @@ describe AssetHostCore::Loaders::URL do
     it 'creates and returns an asset' do
       loader = AssetHostCore::Loaders::URL.build_from_url('http://imgur.com/a/whatever.jpg')
       asset = loader.load
-      puts "Errors"
-      puts asset.errors.full_messages
+      puts assets.to_yaml
       asset.persisted?.should eq true
       asset.image.file?.should eq true
     end
