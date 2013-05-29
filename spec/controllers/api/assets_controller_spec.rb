884bba4fced4f0535333b3b740f130fce849f44e
Bryan Ricker <bricker88@gmail.com>
2013-05-28 22:00:37 -0700

    Add spec

--- spec/controllers/api/assets_controller_spec.rb
+++ spec/controllers/api/assets_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe AssetHostCore::Api::AssetsController do
+  describe 'GET show' do
+    it 'returns the asset as json' do
+      asset = create :asset
+      get :show, format: :json, id: asset.id, use_route: :assethost
+      JSON.parse(response.body)["id"].should eq asset.id
+    end
+  end
 end
