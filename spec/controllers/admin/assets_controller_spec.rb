4ed55644d5762d4cef5fb82be45e0889204eb9e2
Bryan Ricker <bricker88@gmail.com>
2013-09-11 15:09:12 -0700

    Fix assets controller spec

--- spec/controllers/admin/assets_controller_spec.rb
+++ spec/controllers/admin/assets_controller_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe AssetHostCore::Admin::AssetsController do
   render_views
 
+  before do
+    @user = create :user
+    controller.stub(:current_user) { @user }
+  end
+
   describe "GET metadata" do
     it 'gets all the assets in the ID param' do
       assets = create_list :asset, 2
-      get :metadata, ids: assets.map(&:id).join(","), use_route: :assethost
+      get :metadata, admin_request_params(ids: assets.map(&:id).join(","))
       assigns(:assets).should eq assets
     end
   end
@@ -15,7 +20,12 @@ describe AssetHostCore::Admin::AssetsController do
     it 'finds the passed-in assets and updates them' do
       assets = create_list :asset, 2
 
-      put :update_metadata, assets: { assets.first.id => { title: "New Title 1" }, assets.last.id => { title: "New Title 2"} }, use_route: :assethost
+      put :update_metadata, admin_request_params(
+        :assets => {
+          assets.first.id => { title: "New Title 1" },
+          assets.last.id => { title: "New Title 2" }
+        }
+      )
 
       assets.first.reload.title.should eq "New Title 1"
       assets.last.reload.title.should eq "New Title 2"
