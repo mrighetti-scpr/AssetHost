930bbe87fab0d8c31b15fdafbe43451152777c2e
Bryan Ricker <bricker88@gmail.com>
2013-09-10 21:39:34 -0700

    Cleanup ApiUser class + specs

--- app/models/asset_host_core/api_user.rb
+++ app/models/asset_host_core/api_user.rb
@@ -2,34 +2,35 @@ require 'securerandom'
 
 module AssetHostCore
   class ApiUser < ActiveRecord::Base
-    TOKEN_LENGTH = 20
-
     has_many :permissions, as: :user
 
-    before_create :generate_api_token, if: -> {
-      self.authentication_token.blank?
+    validates_uniqueness_of :auth_token
+
+    before_create :generate_auth_token, if: -> {
+      self.auth_token.blank?
     }
+
 
     class << self
       def authenticate(auth_token)
-        self.find_by_authentication_token(auth_token)
+        self.find_by_auth_token(auth_token)
       end
     end
 
 
     def may?(ability, resource)
       !!self.permissions.find do |p|
-        p.resource == resource.to_s && p.ability == ability
+        p.resource == resource.to_s && p.ability == ability.to_s
       end
     end
 
 
-    def generate_api_token
-      self.authentication_token = SecureRandom.urlsafe_base64
+    def generate_auth_token
+      self.auth_token = SecureRandom.urlsafe_base64
     end
 
-    def generate_api_token!
-      generate_api_token and save
+    def generate_auth_token!
+      generate_auth_token and save
     end
   end
 end
--- spec/models/api_user_spec.rb
+++ spec/models/api_user_spec.rb
@@ -1,4 +1,47 @@
 require 'spec_helper'
 
 describe AssetHostCore::ApiUser do
+  describe '::authenticate' do
+    it "finds the user by auth token" do
+      user = create :api_user, auth_token: "12345"
+
+      AssetHostCore::ApiUser.authenticate("12345").should eq user
+    end
+  end
+
+  describe "auth token generation" do
+    let(:api_user) { build :api_user }
+    subject { api_user }
+
+    context 'before save' do
+      its(:auth_token) { should be_blank }
+    end
+
+    context 'on create' do
+      before { subject.save! }
+      its(:auth_token) { should be_present }
+    end
+  end
+
+
+  describe '#may?' do
+    let(:api_user) { create :api_user }
+
+    context "when the user has the permission" do
+      subject { api_user }
+
+      before :each do
+        api_user.permissions.create(
+          :ability    => "read",
+          :resource   => "AssetHostCore::Asset"
+        )
+      end
+
+      it { should be_allowed_to :read, "AssetHostCore::Asset" }
+    end
+
+    context "when the user does not have the permission" do
+      it { should_not be_allowed_to :read, "AssetHostCore::Asset" }
+    end
+  end
 end
