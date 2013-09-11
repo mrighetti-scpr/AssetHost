1285cc81c86e4b16e47f86ea8197462bde14c77e
Bryan Ricker <bricker88@gmail.com>
2013-09-10 21:39:47 -0700

    Add attr_accessible and validations to Permission

--- app/models/asset_host_core/permission.rb
+++ app/models/asset_host_core/permission.rb
@@ -1,5 +1,9 @@
 module AssetHostCore
   class Permission < ActiveRecord::Base
+    attr_accessible :resource, :ability
+
     belongs_to :user, polymorphic: true
+
+    validates :resource, :ability, :user_id, presence: true
   end
 end
