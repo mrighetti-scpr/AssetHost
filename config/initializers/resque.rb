352b66b81791a92f7a93f6dd96e1505fb1fa57e0
Ben Titcomb <btitcomb@scpr.org>
2017-04-02 21:57:52 -0700

    Fix mysql connection handling with resque.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,9 +2,9 @@
 
 Resque.redis = "#{Rails.application.secrets["resque"]}/assethost-#{Rails.env}"
 
-# # Every time a job is started, make sure the connection
-# # to MySQL is okay. This avoids the "MySQL server has gone away"
-# # error.
-# Resque.after_fork = Proc.new {
-#   ActiveRecord::Base.verify_active_connections!
-# }
+# Every time a job is started, make sure the connection
+# to MySQL is okay. This avoids the "MySQL server has gone away"
+# error.
+Resque.after_fork = Proc.new {
+  ActiveRecord::Base.clear_active_connections!
+}
