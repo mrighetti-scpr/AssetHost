f3f90d1f61d914ecf13131a479ad38aaf4c6b840
Ben Titcomb <btitcomb@scpr.org>
2017-04-11 20:43:56 -0700

    Someho this needed to come before requiring the application.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-# Load the Rails application.
-require_relative 'application'
-
 # Assigns a Docker host if using Docker -- defaults to localhost
 DOCKER_HOST = ENV['DOCKER_HOST'] ? URI(ENV['DOCKER_HOST']).host : "localhost"
+
+# Load the Rails application.
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
