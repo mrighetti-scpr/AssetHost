45d3aad5b9b44e75d58edba8305a74d9785127e2
Ben Titcomb <btitcomb@scpr.org>
2017-04-18 20:52:20 -0700

    lol wut

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   if config = Rails.configuration.database_configuration[Rails.env]["users"]
     establish_connection(config) if config["host"].present?
-    if table_name = config["table_name"].present?
+    if table_name = config["table_name"]
       self.table_name = table_name
     end
   end
