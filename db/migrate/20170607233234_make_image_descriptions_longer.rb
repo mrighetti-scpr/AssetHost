cb1f71f9baeecbc71f27c461bd0ce292ad853f61
Ben Titcomb <btitcomb@scpr.org>
2018-02-27 15:16:40 -0800

    Remove old migrations.

--- db/migrate/20170607233234_make_image_descriptions_longer.rb
+++
@@ -1,8 +0,0 @@
-class MakeImageDescriptionsLonger < ActiveRecord::Migration[5.0]
-  def up
-    change_column :asset_host_core_assets, :image_description, :text
-  end
-  def down
-    change_column :asset_host_core_assets, :image_description, :string
-  end
-end
--- db/migrate/20171107183955_make_title_larger.rb
+++
@@ -1,8 +0,0 @@
-class MakeTitleLarger < ActiveRecord::Migration[5.0]
-  def up
-    change_column :asset_host_core_assets, :title, :text
-  end
-  def down
-    change_column :asset_host_core_assets, :title, :string
-  end
-end
