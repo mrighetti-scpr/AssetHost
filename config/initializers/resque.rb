78209b5b375668a15cc75f317d0e1c857ac7082c
Ben Titcomb <btitcomb@scpr.org>
2018-06-20 09:40:29 -0700

    Remove erroneous reference to ActiveRecord.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,9 +3,10 @@ unless Rails.env.test?
   Resque.redis = "#{ENV["ASSETHOST_RESQUE_HOST"]}:#{ENV["ASSETHOST_RESQUE_PORT"]}/assethost-#{Rails.env}"
 end
 
-# Every time a job is started, make sure the connection
-# to MySQL is okay. This avoids the "MySQL server has gone away"
-# error.
-Resque.after_fork = Proc.new {
-  ActiveRecord::Base.clear_active_connections!
-}
+# 🚨 Remove this once we're sure this is definitely not going to be needed again.
+# # Every time a job is started, make sure the connection
+# # to MySQL is okay. This avoids the "MySQL server has gone away"
+# # error.
+# Resque.after_fork = Proc.new {
+#   ActiveRecord::Base.clear_active_connections!
+# }
