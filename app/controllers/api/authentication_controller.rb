679e63c111fcc2a1bc4fc969ce5401301114424b
Ben Titcomb <btitcomb@scpr.org>
2018-06-22 15:53:15 -0700

    Fix document not found error in cas authentication action.

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -6,12 +6,12 @@ class Api::AuthenticationController < Api::BaseController
     current_port = ((request.port === 80) || (request.port === 443)) ? "" : request.port
     current_host = "#{request.protocol}#{request.host}:#{current_port}"
     puts "#{current_host}#{request.path}"
-    resp = conn.get '/serviceValidate', { ticket: params[:ticket], service: "#{current_host}#{request.path}" } 
+    resp   = conn.get '/serviceValidate', { ticket: params[:ticket], service: "#{current_host}#{request.path}" } 
     parser = Nori.new(parser: :rexml)
     xml    = parser.parse(resp.body)
     username = xml.dig("cas:serviceResponse", "cas:authenticationSuccess", "cas:user")
     return head(401) unless username
-    @entity = User.find_by(username: username) || User.create(username: username, password: SecureRandom.base64(12))
+    @entity = User.where(username: username).first || User.create(username: username, password: SecureRandom.base64(12))
     redirect_to "#{current_host}/login/?token=#{auth_token}"
   end
 
@@ -33,7 +33,7 @@ class Api::AuthenticationController < Api::BaseController
     return if !current_user || !current_user.can?("users", "write") || !params[:id]
     user    = User.find(params[:id])
     payload = { sub: user.id }
-    token = JWT.encode(payload, Rails.application.config.secret_key_base, "HS256")
+    token   = JWT.encode(payload, Rails.application.config.secret_key_base, "HS256")
     render json: {jwt: token}.to_json
   end
 
