f8d4c439eea308833a2bd48e21e9432bae2d2202
arellaTV <jeremiah.arella@gmail.com>
2018-07-24 14:05:15 -0700

    Adds new chooser features and bugfixes

--- frontend/app/components/editor-dialog.js
+++ frontend/app/components/editor-dialog.js
@@ -11,22 +11,21 @@ export default Component.extend({
   },
   actions: {
     saveAndClose(){
-      const asset                = this.get('asset'),
-            shouldPersistCaption = asset.get('shouldPersistCaption');
-      if(!shouldPersistCaption) return this.onClose();
+      const asset = this.get('asset');
       this.get('store')
           .findRecord('asset', asset.id)
           .then(record => {
-            if(shouldPersistCaption) record.set('caption', asset.get('caption'));
+            record.setProperties(asset);
             return record.save()
-                         .then(() => {
-                           this.onClose();
-                           if(shouldPersistCaption) this.get('paperToaster').show('Caption successfully saved back to AssetHost.', { toastClass: 'application-toast' });
-                           this.get('paperToaster').show('Asset saved successfully.', { toastClass: 'application-toast' });
-                         });
+              .then(() =>{
+                this.onClose();
+                this.get('paperToaster').show('Asset saved successfully.', { toastClass: 'application-toast' });
+              })
+              .catch(() => {
+                this.get('paperToaster').show('Failed to save asset.', { toastClass: 'application-toast' });
+              });
           })
           .catch(() => {
-            if(shouldPersistCaption) this.get('paperToaster').show('Failed to save caption back to AssetHost.', { toastClass: 'application-toast' });
             this.get('paperToaster').show('Failed to save asset.', { toastClass: 'application-toast' });
           });
     }
